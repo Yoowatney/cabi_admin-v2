e4f4dc91a3e269ee4c0a095124c6da99ed71ac5a
joohongpark <daleaderprofq@gmail.com>
2022-08-18 00:03:33 +0900

    feat: #6 apply config module

---
+++ src/config/configuration.ts
@@ -0,0 +1,9 @@
+export default () => ({
+  database: {
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT, 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+  },
+});
