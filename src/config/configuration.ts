bb787f6e68b36a616296cd061108fa63c8896c80
joohongpark <daleaderprofq@gmail.com>
2022-08-20 18:55:52 +0900

    feat: #12 add auth module & middleware(guard)

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -6,4 +6,12 @@ export default () => ({
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
   },
+  admin: {
+    id: process.env.ADMIN_ID,
+    password: process.env.ADMIN_PASSWORD,
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRES,
+  },
 });
