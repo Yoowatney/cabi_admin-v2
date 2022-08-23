e00d7284b4d3bc8bc277c3d542bfeda9bdb5fa49
kyoshong <qkrgytjd0827@naver.com>
2022-08-23 19:13:04 +0900

    merge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,15 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { ActivationModule } from './activation/activation.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { CabinetModule } from './cabinet/cabinet.module';
-import configuration from './config/configuration';
 import { LentModule } from './lent/lent.module';
 import { ReturnModule } from './return/return.module';
 import { SearchModule } from './search/search.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      load: [configuration],
-      isGlobal: true, // TODO: remove after
-    }),
     ActivationModule,
     AuthModule,
     CabinetModule,
--- src/search/search.module.ts
+++ src/search/search.module.ts
@@ -1,16 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ISearchRepository } from './repository/ISearchRepository';
-import { RawquerySearchRepository } from './repository/rawquery-search.repository';
 import { SearchController } from './search.controller';
-import { SearchService } from './search.service';
-
-const repo = {
-  provide: ISearchRepository,
-  useClass: RawquerySearchRepository,
-};
 
 @Module({
   controllers: [SearchController],
-  providers: [SearchService, repo],
 })
 export class SearchModule {}
